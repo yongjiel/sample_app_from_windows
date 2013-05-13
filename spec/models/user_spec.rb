123f5f10aae22f7366f73b67435f48cac213388e
yongjiel <yongjiel@ualberta.ca>
2013-05-12 21:49:50 -0600

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,9 @@ describe User do
   it { should respond_to(:email) }
 	it { should respond_to(:password_digest)}
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
+	
 		it { should be_valid }
 	describe "when password is not present" do
   	before { @user.password = @user.password_confirmation = " " }
@@ -97,5 +100,9 @@ describe User do
       @user.reload.email.should == mixed_case_email.downcase
     end
   end
-	
+ describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+		
 end
