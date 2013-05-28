6d4150330458ecaaf46d5853fd14033ad7480f92
yongjiel <yongjiel@ualberta.ca>
2013-05-27 23:38:12 -0600

    finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,7 @@ class SessionsController < ApplicationController
 	  if user && user.authenticate(params[:session][:password])
   	  # Sign the user in and redirect to the user's show page.
 			sign_in user
-			flash[:success] = "Welcome to the Sample App!"
-      redirect_to user
+      redirect_back_or user
 			
 	  else
   	  # Create an error message and re-render the signin form.
